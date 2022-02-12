a786d94e36eb406d0fa61badccebfcc5c7198330
meltyyyyy <takeru.abe@porto-seguro.us-central1-a.c.kaggle-meltyyyyy.internal>
2022-02-12 13:54:04 +0000

    cross-validatioin

--- utils/train.py
+++ utils/train.py
@@ -3,6 +3,8 @@ import numpy as np
 
 from sklearn.linear_model import LogisticRegression
 from logging import StreamHandler, DEBUG, Formatter, FileHandler, getLogger
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import log_loss, roc_auc_score
 
 from load_data import load_train_data, load_test_data
 
@@ -36,6 +38,30 @@ if __name__ == '__main__':
 
     logger.debug('train columns: {} {}'.format(use_cols.shape, use_cols))
     logger.info('train data loaded {}'.format(X_train.shape))
+
+    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
+
+    list_auc_score = []
+    list_logloss_score = []
+
+    for train_idx,valid_idx in cv.split(X_train,y_train):
+        trn_x = X_train.iloc[train_idx,:]
+        val_x = X_train.iloc[valid_idx,:]
+
+        trn_y = y_train[train_idx]
+        val_y = y_train[valid_idx]
+        clf = LogisticRegression(random_state=0)
+        clf.fit(X_train,y_train)
+
+        pred = clf.predict_proba(val_x)[:,1]
+        sc_logloss = log_loss(val_y,pred)
+        sc_auc = roc_auc_score(val_y,pred)
+
+        list_logloss_score.append(sc_logloss)
+        list_auc_score.append(sc_auc)
+        logger.debug('   logloss: {}, auc: {}'.format(sc_logloss,sc_auc))
+
+    logger.info('logloss: {}, auc: {}'.format(np.mean(list_logloss_score),np.mean(list_auc_score)))
     clf = LogisticRegression(random_state=0)
     clf.fit(X_train, y_train)
 
@@ -46,7 +72,7 @@ if __name__ == '__main__':
     X_test = df[use_cols].sort_values('id')
 
     logger.info('test data loaded {}'.format(X_test.shape))
-    pred_test = clf.predict_proba(X_test)
+    pred_test = clf.predict_proba(X_test)[:,1]
 
     df_submit = pd.read_csv(SAMPLE_SUBMIT_FILE).sort_values('id')
     df_submit['target'] = pred_test
